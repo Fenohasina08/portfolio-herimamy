81bf0d2f319c29171484617e1427b5647f6d1d53
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-06-06 16:52:04 +0300

    feat(Certifications): create certifications section

---
+++ src/components/sections/Certifications.jsx
@@ -0,0 +1,9 @@
+const Certifications = () => {
+  return (
+    <section id="certifications" className="certifications-section">
+        <h2>Certifications</h2>
+    </section>
+  );
+};
+
+export default Certifications;
